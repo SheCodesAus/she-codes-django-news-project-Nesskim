9791f7b9e363d4b8a4cfaa56d9c4fd761681e2ac
Vanessa <102584523+Nesskim@users.noreply.github.com>
2022-06-04 11:40:57 +1000

    submit to main - laptop dying

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,6 +1,14 @@
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import NewsStory
+from .forms import StoryForm
 
+
+class AddStoryView(generic.CreateView):
+    form_class = Story
+    Formcontext_object_name = 'storyForm'
+    template_name = 'news/createStory.html'
+    success_url = reverse_lazy('news:index')
 
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
@@ -14,3 +22,8 @@ class IndexView(generic.ListView):
         context['latest_stories'] = NewsStory.objects.all()[:4]
         context['all_stories'] = NewsStory.objects.all()
         return context
+
+class StoryView(generic.DetailView):
+    model = NewsStory
+    template_name = 'news/story.html'
+    context_object_name = 'story'
